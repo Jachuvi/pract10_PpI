94a4f24c7a734fdb8b642195acc0a6a3ecb61580
JCV <jachuvi200@gmail.com>
2025-11-06 22:56:03 -0600

    Se creo el elemento <form> y se ajusto la barra de navegación

--- registro.php
+++ registro.php
@@ -8,38 +8,54 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="container"> 
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"> 
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"> 
-                <svg class="bi me-2" width="40" height="32" aria-hidden="true">
-                    <use xlink:href="#bootstrap">
-
-                    </use></svg> 
-                    <span class="fs-4">Práctica 10</span> 
-            </a> 
-            <ul class="nav nav-pills"> 
-                <li class="nav-item">
-                    <a href="#" class="nav-link active" aria-current="page">Inicio</a>
-                </li> 
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Registro</a>
-                </li> 
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Consulta</a>
-                </li> 
-                <li class="nav-item">
-                    <a href="#" class="nav-link">FAQs</a>
-                </li> 
-                <li class="nav-item">
-                    <a href="#" class="nav-link">About</a>
-                </li> 
-            </ul> 
-        </header> 
-    </div>
-    <div class="container-md p-5 my-5 border">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar w/ text</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                    Navbar text with an inline element
+                </span>
+            </div>
+        </div>
+    </nav>
+    <form action="registroDB.php" method="POST" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="pk" class="form-label">PK</label>
+            <input class="form-control" type="number" placeholder="Default input" aria-label="default input example" name="pk" id="pk">
+        </div>
+        
+        <label for="titulo" class="form-label">Título</label>
+        <input class="form-control" type="text" placeholder="Default input" aria-label="default input example" name="titulo" id="titulo">
+         <label for="autor" class="form-label">Autor</label>
+        <input class="form-control" type="text" placeholder="Default input" aria-label="default input example" name="autor" id="autor">
+        <label for="estreno" class="form-label">Estreno</label>
+        <input type="date" name="estreno" id="estreno">
+        <div class="mb-3">
+            <label for="portada" class="form-label">Subir imagen</label>
+            <input class="form-control" type="file" id="portada" name="portada" accept="image/*">
+        </div>
+        <button type="submit" class="btn btn-primary">
+            Subir
+        </button>
         
 
-    </div>
+    </form>
+
+    
     
     
 </body>
